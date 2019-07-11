d42a60839379fecd45ac33e6e12e47996dff7a51
kacperadach <kacperadach@gmail.com>
2019-07-11 10:35:03 -0400

    work

--- src/components/socket/socket.js
+++ src/components/socket/socket.js
@@ -1,28 +1,28 @@
 'use es6';
 
 import React, { useState, useEffect } from 'react';
-import {connectToSocket} from '../../api/socket';
+import socketIOClient  from 'socket.io-client';
 
+const socket = socketIOClient('http://127.0.0.1:5000');
+socket.on('connect', () => console.log('connected'));
+socket.on('disconnect', () => console.log('disconnect'));
 
 function Socket() {
-    
-    const [connected, setConnected] = useState(false);
-    const [attemptedConnect, setAttemptedConnect] = useState(false);
-    const [socket, setSocket] = useState(null);
+    const [lastSent, setLastSent] = useState(0);
+    const [lastReceived, setLastReceived] = useState(0);
 
-    if (!connected) {
-        connectToSocket((data) => console.log(data));
-        setConnected(true);
-    }
+    useEffect(() => {
+        socket.send('test');
+        socket.on('event', () => setLastReceived(Date.now()));
+    }, []);
 
-
-    
-
-    return <div>socket</div>;
+    return (
+        <div>
+            <p>{lastSent}</p>
+            <p>{lastReceived}</p>
+        </div>
+    );
 }
 
-function setUpSocket() {
-
-}
 
 export default Socket;
