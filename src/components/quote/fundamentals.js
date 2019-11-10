83d8bd1b100a7e54b242c65ae424bdeb3f21c063
Kacper Adach <kadach@hubspot.com>
2019-11-10 12:14:59 -0500

    more werk

--- src/components/quote/fundamentals.js
+++ src/components/quote/fundamentals.js
@@ -19,16 +19,23 @@ function Stats({ stats }) {
 
     const groups = splitIntoGroups(Object.entries(stats));
     return (
-        <table>
-            <thead>Stats</thead>
-            <tbody>
-                    {groups.map(group => {
-                        return <tr>{group.map(pair => {
-                            return <td>{pair[0]}: {pair[1]}</td>
-                        })}</tr>
-                    })}
-            </tbody>
-        </table>
+        <div style={{width: 'fit-content'}} className="bg-gray-300 m-4 p-3">
+            <table className="bg-white">
+                <thead>Stats</thead>
+                <tbody className="font-thin">
+                        {groups.map(group => {
+                            return <tr className="flex">{group.map(pair => {
+                                return (
+                                    <div>
+                                        <td style={{width: '95px'}} className="bg-gray-100 border-black border-1 text-sm">{pair[0]}</td>
+                                        <td style={{width: '95px'}} className="bg-gray-200 border-black border-1 text-sm">{pair[1]}</td>
+                                    </div>
+                                )
+                            })}</tr>
+                        })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
@@ -36,8 +43,8 @@ function splitIntoGroups(stats) {
     const length = stats.length;
 
     const split = [];
-    for (let i = 0; i < Math.ceil(length / 10); i++) {
-        split.push(stats.slice(i * 10, (i * 10) + 10));
+    for (let i = 0; i < Math.ceil(length / 8); i++) {
+        split.push(stats.slice(i * 8, (i * 8) + 8));
     }
     
     return split;
@@ -45,36 +52,38 @@ function splitIntoGroups(stats) {
 
 function Insider({ insider }) {
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <th>Transaction</th>
-                    <th>Date</th>
-                    <th>Entity</th>
-                    <th>Relationship</th>
-                    <th>Shares</th>
-                    <th>Cost</th>
-                    <th>Value</th>
-                    <th>Total Shares</th>
-                    <th>SEC</th>
-                </tr>
-                {insider.map(i => {
-                    return (
-                        <tr>
-                            <td>{i.transaction}</td>
-                            <td>{i.date_executed}</td>
-                            <td>{i.entity}</td>
-                            <td>{i.relationship}</td>
-                            <td>{i.num_shares}</td>
-                            <td>${i.cost}</td>
-                            <td>${i.dollar_value}</td>
-                            <td>{i.shares_total}</td>
-                            <td><a href={i.sec_form_link}>{i.sec_form_filed_at}</a></td>
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+        <div style={{width: 'fit-content'}} className="bg-gray-300 m-4 p-3">
+            <table className="bg-white">
+                <tbody className="font-thin">
+                    <tr className="text-left">
+                        <th className="p-2">Transaction</th>
+                        <th className="p-2">Date</th>
+                        <th className="p-2">Entity</th>
+                        <th className="p-2">Relationship</th>
+                        <th className="p-2">Shares</th>
+                        <th className="p-2">Cost</th>
+                        <th className="p-2">Value</th>
+                        <th className="p-2">Total Shares</th>
+                        <th className="p-2">SEC</th>
+                    </tr>
+                    {insider.map(i => {
+                        return (
+                            <tr className="w-12">
+                                <td className="w-40">{i.transaction}</td>
+                                <td className="w-24">{i.date_executed}</td>
+                                <td className="w-64">{i.entity}</td>
+                                <td className="w-64">{i.relationship}</td>
+                                <td>{i.num_shares}</td>
+                                <td className="w-24">${i.cost}</td>
+                                <td className="w-24">${i.dollar_value}</td>
+                                <td>{i.shares_total}</td>
+                                <td><a className="text-blue-500" href={i.sec_form_link}>{i.sec_form_filed_at}</a></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        'tiny': '0.5rem'
+      },
+      width: {
+        'test': '95px'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
